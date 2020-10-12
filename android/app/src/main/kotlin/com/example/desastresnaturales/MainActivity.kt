9bd045a1e2f2904720ae7a7786087e425b381931
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2020-10-12 11:05:55 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/desastresnaturales/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.desastresnaturales
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:provider/provider.dart';
+
+import 'authentication_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+
+String str_email = "";
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) =>
+          context.read<AuthenticationService>().authStateChanges,
+        ),
+      ],
+      child: MaterialApp(
+        home: AuthenticationWrapper(),
+      ),
+    );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  const AuthenticationWrapper({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final firebaseuser = context.watch<User>();
+    if (firebaseuser != null) {
+      if (str_email == "administrador@gmail.com") {
+        Fluttertoast.showToast(msg: "Bienvenido Administrador");
+        //return ListViewProduct();
+      }
+      Fluttertoast.showToast(msg: "Bienvenido Consultor");
+      //return ListViewProduct_user();
+    }
+    Fluttertoast.showToast(msg: "Ingresar datos correspondientes");
+    return Login_page();
+  }
+}
+
+class Login_page extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ingresar'),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+        body: Form(
+            key: _formkey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(labelText: "Correo"),
+                ),
+                TextFormField(
+                  controller: passwordController,
+                  decoration: InputDecoration(labelText: "Contraseña"),
+                  obscureText: true,
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    str_email = emailController.text.trim();
+                    context.read<AuthenticationService>().signIn(
+                        email: emailController.text.trim(),
+                        password: passwordController.text.trim());
+                  },
+                  child: Text('Ingresar'),
+                )
+              ],
+            )),
+      ),
+    );
+  }
+}
+
+
