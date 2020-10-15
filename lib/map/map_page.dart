93c0ede4cf6848ba121fc8c3251bf8818cdd273c
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2020-10-15 14:06:51 -0500

    Marker en el mapa listo

--- lib/map/map_page.dart
+++ lib/map/map_page.dart
@@ -1,48 +1,35 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location_permissions/location_permissions.dart';
+import 'locations.dart' as locations;
 
-class Map_page extends StatelessWidget {
-  PermissionStatus permission;
+void main() {
+  runApp(Map_page());
+}
+final LatLng _center = const LatLng(19.775405, -99.207922);
 
-  Map<String, Marker> markers = {};
+class Map_page extends StatefulWidget {
 
-  Marker m1;
+  @override
+  _Map_pageState createState() => _Map_pageState();
+}
 
-  MarkerId markerId2;
+class _Map_pageState extends State<Map_page> {
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      final marker = Marker(
+        markerId: MarkerId("1"),
+        position: LatLng(19.775405, -99.207922),
+        infoWindow: InfoWindow(
+          title: "aculco",
+        ),
+      );
+      _markers["1"] = marker;
 
-  MarkerId markerId3;
-  Marker marker2;
-  Marker marker3;
 
-  GoogleMapController mapController;
-  final LatLng _center = const LatLng(19.775405, -99.207922);
-
-  void _onMapCreated(GoogleMapController controller) async {
-    permission = await LocationPermissions().requestPermissions();
-
-    mapController = controller;
-
-    markerId2 = MarkerId("2");
-    markerId3 = MarkerId("3");
-
-    marker2 = Marker(
-      markerId: markerId2,
-      position: LatLng(19.775405, -99.207922),
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
-    );
-    marker3 = Marker(
-      markerId: markerId3,
-      position: LatLng(17.9689, 79.5941),
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
-    );
-    markers.clear();
-    markers["2"] = marker2;
-    markers["3"] = marker3;
-
-    Fluttertoast.showToast(msg: "-" + markers.keys.toString() + "-");
+    });
   }
 
   @override
@@ -50,16 +37,16 @@ class Map_page extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Maps Sample App'),
+          title: const Text('Google Office Locations'),
           backgroundColor: Colors.green[700],
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
-            target: const LatLng(0, 0),
-            zoom: 2,
+            target: _center,
+            zoom: 5,
           ),
-          markers: markers.values.toSet(),
+          markers: _markers.values.toSet(),
         ),
       ),
     );
