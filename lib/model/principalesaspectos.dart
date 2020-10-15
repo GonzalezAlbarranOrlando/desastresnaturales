2c15a78e0d6ff964fa48274e059e9a3696c50481
arianaglez20 <arianaglez20@users.noreply.github.com>
2020-10-15 00:04:35 -0500

    tablas

--- lib/model/principalesaspectos.dart
+++ lib/model/principalesaspectos.dart
@@ -2,7 +2,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class Principales_aspectos{
-  String _id;
+  String _id_igecem;
   String _elevacion;
   String _rio_canal;
   String _cuerpo;
@@ -11,7 +11,7 @@ class Principales_aspectos{
   String _menos_poblado;
   String _industrializados;
 
-  Principales_aspectos(this._id, this._elevacion, this._rio_canal, this._cuerpo, this._mas_poblados, this._mas_extenso, this._menos_poblado, this._industrializados);
+  Principales_aspectos(this._id_igecem, this._elevacion, this._rio_canal, this._cuerpo, this._mas_poblados, this._mas_extenso, this._menos_poblado, this._industrializados);
 
   Principales_aspectos.map(dynamic obj){
     this._elevacion = obj['elevacion'];
@@ -19,21 +19,21 @@ class Principales_aspectos{
     this._cuerpo = obj['cuerpo'];
     this._mas_poblados = obj['maspoblados'];
     this._mas_extenso = obj['masextenso'];
-    this._menos_poblado = obj['clima'];
-    this._industrializados = obj['localización'];
+    this._menos_poblado = obj['menospoblado'];
+    this._industrializados = obj['industrializado'];
   }
 
-  String get id_igecem => _id;
-  String get nombre =>_elevacion;
-  String get significado => _rio_canal;
-  String get cabeceramun => _cuerpo;
-  String get superficie => _mas_poblados;
+  String get id_igecem => _id_igecem;
+  String get elevacion =>_elevacion;
+  String get rio_canal => _rio_canal;
+  String get cuerpo => _cuerpo;
+  String get mas_poblado => _mas_poblados;
   String get altitud => _mas_extenso;
   String get clima => _menos_poblado;
   String get localizacion => _industrializados;
 
   Principales_aspectos.fromSnapShop(DataSnapshot snapshot){
-    _id = snapshot.key;
+    _id_igecem = snapshot.key;
     _elevacion = snapshot.value['nombre'];
     _rio_canal = snapshot.value['significado'];
     _cuerpo = snapshot.value['cabeceramun'];
