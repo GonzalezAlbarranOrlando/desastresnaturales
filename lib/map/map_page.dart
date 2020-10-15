280c83c3fd137f6ec8ec8781865d2c39806933d8
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2020-10-15 12:28:42 -0500

    intento: marcadores en el mapa

--- lib/map/map_page.dart
+++ lib/map/map_page.dart
@@ -1,16 +1,48 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location_permissions/location_permissions.dart';
 
 class Map_page extends StatelessWidget {
+  PermissionStatus permission;
+
+  Map<String, Marker> markers = {};
+
+  Marker m1;
+
+  MarkerId markerId2;
+
+  MarkerId markerId3;
+  Marker marker2;
+  Marker marker3;
 
   GoogleMapController mapController;
-
   final LatLng _center = const LatLng(19.775405, -99.207922);
 
-  void _onMapCreated(GoogleMapController controller) {
+  void _onMapCreated(GoogleMapController controller) async {
+    permission = await LocationPermissions().requestPermissions();
+
     mapController = controller;
+
+    markerId2 = MarkerId("2");
+    markerId3 = MarkerId("3");
+
+    marker2 = Marker(
+      markerId: markerId2,
+      position: LatLng(19.775405, -99.207922),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
+    );
+    marker3 = Marker(
+      markerId: markerId3,
+      position: LatLng(17.9689, 79.5941),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
+    );
+    markers.clear();
+    markers["2"] = marker2;
+    markers["3"] = marker3;
+
+    Fluttertoast.showToast(msg: "-" + markers.keys.toString() + "-");
   }
 
   @override
@@ -24,9 +56,10 @@ class Map_page extends StatelessWidget {
         body: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+            target: const LatLng(0, 0),
+            zoom: 2,
           ),
+          markers: markers.values.toSet(),
         ),
       ),
     );
--- lib/ui/municipios_information.dart
+++ lib/ui/municipios_information.dart
@@ -1,8 +1,11 @@
+
+
 import 'package:desastresnaturales/map/map_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:desastresnaturales/model/municipio.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:location_permissions/location_permissions.dart';
 
 import 'menu_page.dart';
 
@@ -88,7 +91,11 @@ class _MunicipioInformationState extends State<MunicipioInformation> {
                   new Text("Industrializado : ${widget.municipio.lat}", style: TextStyle(fontSize: 18.0),),
                   Padding(padding: EdgeInsets.only(top: 8.0),),
                   Divider(),
-                  RaisedButton(onPressed:  () => Navigator.push(context, MaterialPageRoute(builder: (context) => Map_page())),child: Text("Ubicación"),)
+                  RaisedButton(onPressed:  () {
+                    LocationPermissions().openAppSettings().then((bool hasOpened) =>
+                        debugPrint('App Settings opened: ' + hasOpened.toString()));
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => Map_page()));
+                    },child: Text("Ubicación"),)
                 ],
               ),
             ),
@@ -96,5 +103,121 @@ class _MunicipioInformationState extends State<MunicipioInformation> {
         ),
       ),
     );
+  }
+
+
+  List<Widget> createWidgetList() {
+    final List<Widget> widgets = LocationPermissionLevel.values
+        .map<Widget>((LocationPermissionLevel level) => PermissionWidget(level))
+        .toList();
+
+      widgets.add(StreamingStatusWidget());
+
+
+    return widgets;
+  }
+}
+
+class StreamingStatusWidget extends StatelessWidget {
+  final Stream<ServiceStatus> statusStream =
+      LocationPermissions().serviceStatus;
+
+  @override
+  Widget build(BuildContext context) => ListTile(
+    title: const Text('ServiceStatus'),
+    subtitle: StreamBuilder<ServiceStatus>(
+      stream: statusStream,
+      initialData: ServiceStatus.unknown,
+      builder: (_, AsyncSnapshot<ServiceStatus> snapshot) =>
+          Text('${snapshot.data}'),
+    ),
+  );
+}
+
+class PermissionWidget extends StatefulWidget {
+  const PermissionWidget(this._permissionLevel);
+
+  final LocationPermissionLevel _permissionLevel;
+
+  @override
+  _PermissionState createState() => _PermissionState(_permissionLevel);
+}
+
+class _PermissionState extends State<PermissionWidget> {
+  _PermissionState(this._permissionLevel);
+
+  final LocationPermissionLevel _permissionLevel;
+  PermissionStatus _permissionStatus = PermissionStatus.unknown;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _listenForPermissionStatus();
+  }
+
+  void _listenForPermissionStatus() {
+    final Future<PermissionStatus> statusFuture =
+    LocationPermissions().checkPermissionStatus();
+
+    statusFuture.then((PermissionStatus status) {
+      setState(() {
+        _permissionStatus = status;
+      });
+    });
+  }
+
+  Color getPermissionColor() {
+    switch (_permissionStatus) {
+      case PermissionStatus.denied:
+        return Colors.red;
+      case PermissionStatus.granted:
+        return Colors.green;
+      default:
+        return Colors.grey;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(_permissionLevel.toString()),
+      subtitle: Text(
+        _permissionStatus.toString(),
+        style: TextStyle(color: getPermissionColor()),
+      ),
+      trailing: IconButton(
+          icon: const Icon(Icons.info),
+          onPressed: () {
+            checkServiceStatus(context, _permissionLevel);
+          }),
+      onTap: () {
+        requestPermission(_permissionLevel);
+      },
+    );
+  }
+
+  void checkServiceStatus(
+      BuildContext context, LocationPermissionLevel permissionLevel) {
+    LocationPermissions()
+        .checkServiceStatus()
+        .then((ServiceStatus serviceStatus) {
+      final SnackBar snackBar =
+      SnackBar(content: Text(serviceStatus.toString()));
+
+      Scaffold.of(context).showSnackBar(snackBar);
+    });
+  }
+
+  Future<void> requestPermission(
+      LocationPermissionLevel permissionLevel) async {
+    final PermissionStatus permissionRequestResult = await LocationPermissions()
+        .requestPermissions(permissionLevel: permissionLevel);
+
+    setState(() {
+      print(permissionRequestResult);
+      _permissionStatus = permissionRequestResult;
+      print(_permissionStatus);
+    });
   }
 }
